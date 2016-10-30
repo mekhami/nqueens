99e1339c78dba814f3eacac6cb87d47a89e4b035
Lawrence Vanderpool <lawrence.vanderpool@gmail.com>
2016-10-30 18:33:13 -0500

    remove all the old code

--- queens/nqueens.py
+++ queens/nqueens.py
@@ -12,34 +12,7 @@ The board structure is:
 Rows in this grid are the domain of each row. IE in the row [0, 1, 2] the queen can be placed on the
 1st, 2nd, or 3rd column.
 """
-import copy
 import itertools
-
-
-def backtrack_start(number):
-    """
-    The wrapper around the recursive function to start the stack
-    param number: number of queens to solve for (also size of grid!)
-    """
-    grid = [list(range(number)) for x in range(number)]
-    return backtrack(grid)
-
-
-def backtrack(grid):
-    """
-    The recursive function to solve n-queens with forward checking
-    and arc consistency.
-    """
-    if complete(grid):
-        return grid
-    row_index = grid.index(next((row for row in grid if len(row) != 1), None))
-    for column in grid[row_index]:
-        grid_copy = propagate_constraints(copy.deepcopy(grid), row_index, column)
-        print(grid_copy)
-        new_grid = backtrack(grid_copy)
-        if new_grid is not None:
-            if not fails(new_grid):
-                return new_grid
 
 
 def arc_consistency(grid):
@@ -81,49 +54,6 @@ def fails_constraints(head, tail):
     pass
 
 
-def propagate_constraints(grid, row_index, value):
-    """
-    Take a grid, return the grid that will be the result of enforcing
-    all the constraints on that grid. (Diagonal, vertical, horizontal)
-    The horizontal constraint is implicit when you select a column for
-    the row, so only horizontal and diagonal is needed.
-    """
-    remove_vertical(grid, value)
-    remove_diagonal(grid, row_index, value)
-
-    # Assign the correct row value since we removed it as part of
-    # the above operations.
-    grid[row_index] = [value]
-    return grid
-
-
-def remove_diagonal(grid, row_index, value):
-    """
-    Remove the values diagonally in the grid
-    """
-    for row, _ in enumerate(grid):
-        try:
-            grid[row].remove(value+(row-row_index))
-        except ValueError:
-            pass
-
-        try:
-            grid[row].remove(value-(row-row_index))
-        except ValueError:
-            pass
-
-
-def remove_vertical(grid, value):
-    """
-    Remove the value vertically in the grid
-    """
-    for row in grid:
-        try:
-            row.remove(value)
-        except ValueError:
-            pass
-
-
 def fails(grid):
     """
     Fails a grid if any of the rows have no possible solutions
@@ -141,4 +71,4 @@ def complete(grid):
 
 
 if __name__ == '__main__':
-    print(backtrack_start(6))
+    print("Not implemented yet.")
